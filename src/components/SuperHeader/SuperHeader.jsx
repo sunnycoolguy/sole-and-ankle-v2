1ad20a7a9a5d31f11a04a75ed87cf9ed7e783f38
Sunny <senay_berihu@outlook.com>
2025-03-25 14:45:47 +0000

    feat: style superheader according to Figma

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -26,10 +26,15 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  gap: 24px;
+  padding-block: 12px;
+  padding-inline: 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
